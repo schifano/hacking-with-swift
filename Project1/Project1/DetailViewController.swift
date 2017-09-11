70ce25a07d6aa1c6449786939d695d066319bfe0
Rachel Schifano <rschifa1@gmail.com>
2017-09-11 01:13:45 -0500

    Add DetailView and hidesBarOnTap

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -19,4 +19,14 @@ class DetailViewController: UIViewController {
             imageView.image  = UIImage(named: imageToLoad)
         }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 }
